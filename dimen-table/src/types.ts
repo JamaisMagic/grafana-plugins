f4ecba9eb714854dd0b7f148cca444a8a3f1c9f0
Jamais <wuweikang@cmcm.com>
2020-06-11 15:01:34 +0800

    Add table code.

--- dimen-table/src/types.ts
+++ dimen-table/src/types.ts
@@ -1,7 +1,17 @@
-type SeriesSize = 'sm' | 'md' | 'lg';
+import { TableSortByFieldState } from '@grafana/ui';
 
-export interface SimpleOptions {
-  text: string;
-  showSeriesCount: boolean;
-  seriesCountSize: SeriesSize;
+export interface Options {
+  frameIndex: number;
+  showHeader: boolean;
+  sortBy?: TableSortByFieldState[];
+}
+
+export interface TableSortBy {
+  displayName: string;
+  desc: boolean;
+}
+
+export interface CustomFieldConfig {
+  width: number;
+  displayMode: string;
 }
